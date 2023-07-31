628031e585d0ccacc26ebdd78e5933bdb3778062
Hello World <88145693+byte1001@users.noreply.github.com>
2023-08-01 06:52:26 +0800

    Update demoapp.py

--- demoapp.py
+++ demoapp.py
@@ -2,9 +2,9 @@ from redis2streamlit import Redis2Streamlit
 import streamlit as st
 import os
 
-dhost = "redis-10875.c309.us-east-2-1.ec2.cloud.redislabs.com"
-dusername = "connections-hackathon"
-dpassword = "Redis@Streamlit2023"
+dhost = os.environ['DBHOST']
+dusername = os.environ['DBUSERNAME']
+dpassword = os.environ['DBPASSWORD']
 
 testdb = st.experimental_connection(name="Test", type=Redis2Streamlit)
 testdb._connect(host=dhost, username=dusername, password=dpassword, port=10875)
