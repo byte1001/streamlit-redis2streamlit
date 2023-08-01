b4fbbee4781f95938b6b62c9a9625448eb4faa11
Hello World <88145693+byte1001@users.noreply.github.com>
2023-08-01 17:15:53 +0800

    demoapp.py finished version

--- demoapp.py
+++ demoapp.py
@@ -1,31 +1,30 @@
 # Probably the most inefficient way to convert ASCII to Binary
 
-
-from redis2streamlit.redis2streamlit import Redis2Streamlit
-import streamlit as st
-import os
-
 dbhost = os.environ['DBHOST']
 dbusername = os.environ['DBUSERNAME']
 dbpassword = os.environ['DBPASSWORD']
 
-ascii2binaryconnection = st.experimental_connection(name="ASCII2Binary", type=Redis2Streamlit)
-ascii2binaryconnection._connect(host=dbhost, username=dbusername, password=dbpassword, port=10875)
-a2b = ascii2binaryconnection.get("a2b-string", "json")
-a2bSymbol = ascii2binaryconnection.get("a2b-symbols", "json")
-st.write(a2b)
-st.write(a2bSymbol)
+dbconnection = st.experimental_connection(name="Text2Binary", type=Redis2Streamlit)
+dbconnection._connect(host=dbhost, username=dbusername, password=dbpassword, port=10875)
+a2b = dbconnection.get("a2b", "json")
+
 
 def ascii2binary(inp):
-  res = ""
+  result = ""
   for i in inp:
-    res += a2bDB[i] + " "
-  return res
+    try:
+        if i == " ": i = "space" # these 2 lines because my Redis DB wont allow spaces and single quotes
+        if i == "'": i = "sq" # these 2 lines because my Redis DB wont allow spaces and single quotes
+        result += a2b[str(i)] + " "
+    except Exception as e:
+        return f"Cannot process input; invalid character: {str(e)[1]}"
+  return result
+
 
 st.title('Streamlit Connections Hackathon')
-st.header('ASCII2Binary - Redis2Streamlit')
-st.write('Probably the most inefficient way to make an ASCII to Binary Converter')
+st.header('Text2Binary - Redis2Streamlit')
+st.write('Probably the most inefficient way to make a Text to Binary Converter')
 st.write('')
-ascii = st.text_input('ASCII here')
+text = st.text_input('Text here', placeholder="Enter text...")
 st.write("Binary:")
-binary = st.code(a2b(ascii))
+binary = st.code(ascii2binary(ascii))
